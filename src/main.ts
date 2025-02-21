07bbc41cd8910dc60ae98b211d7d4d8f1540ccf2
git9527 <398816613@qq.com>
2025-02-21 21:52:42 +0800

    refactor: update Dropbox action to use refresh token and add app key/secret inputs

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,35 @@
+import {DropboxResponse, files} from 'dropbox'
+import FileMetadata = files.FileMetadata
+
 const Dropbox = require('dropbox').Dropbox
 const fs = require('fs')
-const fetch2 = require('node-fetch')
 const core = require('@actions/core')
 const glob = require('glob')
 
-const accessToken = core.getInput('DROPBOX_ACCESS_TOKEN')
+const refreshToken = core.getInput('DROPBOX_REFRESH_TOKEN')
+const clientId = core.getInput('DROPBOX_APP_KEY')
+const clientSecret = core.getInput('DROPBOX_APP_SECRET')
 const globSource = core.getInput('GLOB')
 const dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX')
 const isDebug = core.getInput('DEBUG')
-const dropbox = new Dropbox({accessToken, fetch: fetch2})
+const dropbox = new Dropbox({refreshToken: refreshToken, clientId: clientId, clientSecret: clientSecret})
 
 function uploadMuhFile(filePath: string): Promise<any> {
   const file = fs.readFileSync(filePath)
   const destinationPath = `${dropboxPathPrefix}${filePath}`
-  if (isDebug) console.log('uploaded file to Dropbox at: ', destinationPath)
   // random delay to avoid rate limiting
   const delay = Math.floor(Math.random() * 1000)
   if (isDebug) console.log('delaying for: ', delay)
+  console.log(`uploading file from ${file} to Dropbox: ${destinationPath}`)
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(dropbox
         .filesUpload({path: destinationPath, contents: file})
-        .then((response: any) => {
+        .then((response: DropboxResponse<FileMetadata>) => {
           if (isDebug) console.log(response)
+          if (response.status !== 200) {
+            core.setFailed('File upload failed', response)
+          }
           return response
         })
         .catch((error: any) => {
